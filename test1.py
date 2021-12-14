85907e6aff0b43190a41dbc834fd62fa0c33327a
rzyngier <rafael_zyngier@brown.edu>
2021-12-13 19:37:47 -0500

    asdf

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+from compute_black_scholes import *
 
 aapl = yf.Ticker("AAPL")
 r_f = .01426
@@ -14,16 +15,11 @@ jan_14th_exp = aapl.option_chain('2022-01-14')
 # combine all dataframes into a single dataframe
 df = pd.concat(jan_14th_exp)
 
-# save to csv
-# df.to_csv('ticker.csv')
-
 implied_vol = df['impliedVolatility']
 strike = df['strike']
-
 options_prices = df['lastPrice']
+
+BS_CALL(S, K, T, r, sigma):
 
 
 print(implied_vol)
-
-
-
